2cc238754334eab0115b2aca4e29f5d64805ff48
michelle000tang <michelle000tang@gmail.com>
2025-07-16 10:11:21 -0700

    poop

--- pair-simulation-practice.R
+++ pair-simulation-practice.R
@@ -1,14 +1,15 @@
 # simulate_pair_data.R
 # A collaborative exercise in Git + R
-# Created by: Michelle Tang and 
-# Last updated by: Joss
+# Created by: Michelle Tang and Joss
+# Last updated by: Michelle
 # Date created: 7/16/25
-# Last updated on: 9:47 AM 7/16/25
+# Last updated on: 10:11 AM 7/16/25
 
 # Simulate predictor
 x <- rnorm(100)
 
-test
-
 # Simulate response variable y
 y <- 2 * x + rnorm(100, sd = 0.3)
+
+# Create scatterplot
+plot(x, y, main = "x vs y: Simulated Linear Data")
