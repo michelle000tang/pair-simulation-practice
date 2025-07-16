6e93fc52d9179f22ce026e56cf15ba209be43f2d
josstij <jocelynmtijerina@arizona.edu>
2025-07-16 10:09:39 -0700

    joss update

--- pair-simulation-practice.R
+++ pair-simulation-practice.R
@@ -1,7 +1,7 @@
 # simulate_pair_data.R
 # A collaborative exercise in Git + R
 # Created by: Michelle Tang and 
-# Last updated by: Michelle
+# Last updated by: Joss
 # Date created: 7/16/25
 # Last updated on: 9:47 AM 7/16/25
 
@@ -9,3 +9,6 @@
 x <- rnorm(100)
 
 test
+
+# Simulate response variable y
+y <- 2 * x + rnorm(100, sd = 0.3)
